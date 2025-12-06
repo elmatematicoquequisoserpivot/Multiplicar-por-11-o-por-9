952535b3025938494f70b989cdb6033c15cddc41
Diego Alonso <elmatematicoquequisoserpivot@gmail.com>
2025-12-06 23:52:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -74,14 +74,16 @@ $(document).ready(function () {
     $("#problems").html(`
       <div class="problemStyle">
         <p class="h4">${problem.x} x ${problem.y} = </p>
-        <input type="number" id="answer" class="form-control text-center" placeholder="Tu respuesta" inputmode="numeric">
-        <button id="submit" class="btn btn-success mt-2">Enviar</button>
+          <form id="problemForm">
+            <input type="number" id="answer" class="form-control text-center" placeholder="Tu respuesta" inputmode="numeric">
+            <button type="submit" class="btn btn-success mt-2">Enviar</button>
+        </form>
       </div>
     `) 
     $("#answer").focus();
 
     // Manejar la respuesta del usuario
-   $("#submit").on("click", function() {
+ $("#problemForm").on("submit", function(e) {
       let userAnswer = parseInt($("#answer").val());
       if (userAnswer === problem.correctAnswer) {
         handleCorrectAnswer(problem);
